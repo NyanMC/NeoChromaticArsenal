dfa5532f7c715905a7a23ac27a5ba2a0a13dcad1
ChromaNyan <nyancatz593@gmail.com>
2025-01-18 17:59:29 -0600

    fix: correct fractured color

--- src/main/java/page/chromanyan/chromaticarsenal/mobeffect/FracturedEffect.java
+++ src/main/java/page/chromanyan/chromaticarsenal/mobeffect/FracturedEffect.java
@@ -8,7 +8,7 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 
 public class FracturedEffect extends MobEffect {
     public FracturedEffect() {
-        super(MobEffectCategory.HARMFUL, 0xFFFF00);
+        super(MobEffectCategory.HARMFUL, 0x00FFFF);
         this.addAttributeModifier(Attributes.MAX_HEALTH, ChromaticArsenal.of("fractured_max_health"), -0.1, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL);
     }
 }
